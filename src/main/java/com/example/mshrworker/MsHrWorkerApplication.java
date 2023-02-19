28c775b8dfec6e30fe0b3d9621b28a923ddd5868
Rafael Vieira <rafael.svieira@icloud.com>
2023-02-19 14:51:12 -0300

    initial commit hr-worker

---
+++ src/main/java/com/example/mshrworker/MsHrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mshrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHrWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsHrWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mshrworker/MsHrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mshrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHrWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
